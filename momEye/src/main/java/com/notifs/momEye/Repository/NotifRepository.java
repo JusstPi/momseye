281dc0bcd84e6c7cc2a4b81efa1e6c45ed008898
JustPi <pamela.canedo@cit.edu>
2022-11-17 14:11:07 +0800

    minor controller

--- momEye/src/main/java/com/notifs/momEye/Repository/NotifRepository.java
+++ momEye/src/main/java/com/notifs/momEye/Repository/NotifRepository.java
@@ -5,13 +5,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface NotifRepository extends JpaRepository<NotifRepository, Integer>{
-
-	//the Wrapper class counterpart of the data type of the PK of the Entity
-	
-	//PK = id
-	//data type of id = int
-	//Wrapper class = Integer
 	
 	//user-defined query 
-	NotifRepository findByid(String id);
+	NotifRepository findById(String id);
 }
